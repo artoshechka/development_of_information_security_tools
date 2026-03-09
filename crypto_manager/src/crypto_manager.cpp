0df9cbd8f2d21fc13805ff317816f872254ae573
Artemenko Anton <muroshechka@gmail.com>
2026-03-09 17:37:21 +0700

    UniqueEVP_CIPHER_CTX

--- crypto_manager/src/crypto_manager.cpp
+++ crypto_manager/src/crypto_manager.cpp
@@ -27,6 +27,21 @@ static QByteArray deriveKey(const QString &userPassword)
     return QCryptographicHash::hash(userPassword.toUtf8(), QCryptographicHash::Sha256);
 }
 
+/// @brief Структура для автоматического освобождения ресурсов EVP_CIPHER_CTX с помощью std::unique_ptr.
+struct EVP_CIPHER_CTX_Deleter
+{
+    void operator()(EVP_CIPHER_CTX *ctx) const
+    {
+        if (ctx)
+        {
+            EVP_CIPHER_CTX_free(ctx);
+        }
+    }
+};
+
+/// @brief Тип умного указателя для EVP_CIPHER_CTX
+using UniqPtrCipherContext = std::unique_ptr<EVP_CIPHER_CTX, EVP_CIPHER_CTX_Deleter>;
+
 } // namespace
 
 OpenSSLCryptoManager &OpenSSLCryptoManager::Instance()
@@ -50,68 +65,60 @@ bool OpenSSLCryptoManager::EncryptFile(const QString &filePath, const QString &p
 
     QByteArray encryptionKey = deriveKey(password);
 
-    unsigned char initializationVector[16];
+    std::vector<unsigned char> initializationVector(16);
 
-    if (!RAND_bytes(initializationVector, sizeof(initializationVector)))
+    if (!RAND_bytes(initializationVector.data(), initializationVector.size()))
         return false;
 
-    EVP_CIPHER_CTX *cipherContext = EVP_CIPHER_CTX_new();
-
+    UniqPtrCipherContext cipherContext(EVP_CIPHER_CTX_new());
     if (!cipherContext)
+    {
         return false;
+    }
 
-    QByteArray encryptedData(fileContent.size() + 16, 0);
+    QByteArray encryptedData(fileContent.size() + EVP_MAX_BLOCK_LENGTH, 0);
 
     int bytesProcessed = 0;
     int totalCiphertextLength = 0;
 
-    if (!EVP_EncryptInit_ex(cipherContext, EVP_aes_256_cbc(), nullptr,
-                            reinterpret_cast<const unsigned char *>(encryptionKey.data()), initializationVector))
+    if (!EVP_EncryptInit_ex(cipherContext.get(), EVP_aes_256_cbc(), nullptr,
+                            reinterpret_cast<const unsigned char *>(encryptionKey.data()), initializationVector.data()))
     {
-        EVP_CIPHER_CTX_free(cipherContext);
         return false;
     }
 
-    if (!EVP_EncryptUpdate(cipherContext, reinterpret_cast<unsigned char *>(encryptedData.data()), &bytesProcessed,
-                           reinterpret_cast<const unsigned char *>(fileContent.data()), fileContent.size()))
+    if (!EVP_EncryptUpdate(cipherContext.get(), reinterpret_cast<unsigned char *>(encryptedData.data()),
+                           &bytesProcessed, reinterpret_cast<const unsigned char *>(fileContent.data()),
+                           fileContent.size()))
     {
-        EVP_CIPHER_CTX_free(cipherContext);
         return false;
     }
-
     totalCiphertextLength = bytesProcessed;
 
-    if (!EVP_EncryptFinal_ex(cipherContext, reinterpret_cast<unsigned char *>(encryptedData.data()) + bytesProcessed,
-                             &bytesProcessed))
+    if (!EVP_EncryptFinal_ex(cipherContext.get(),
+                             reinterpret_cast<unsigned char *>(encryptedData.data()) + bytesProcessed, &bytesProcessed))
     {
-        EVP_CIPHER_CTX_free(cipherContext);
         return false;
     }
-
     totalCiphertextLength += bytesProcessed;
-
-    EVP_CIPHER_CTX_free(cipherContext);
 
     encryptedData.resize(totalCiphertextLength);
 
     QFile outputFile(filePath);
-
     if (!outputFile.open(QIODevice::WriteOnly))
         return false;
 
     outputFile.write(FILE_MAGIC_SIGNATURE);
-    outputFile.write(reinterpret_cast<char *>(initializationVector), sizeof(initializationVector));
+    outputFile.write(reinterpret_cast<char *>(initializationVector.data()), initializationVector.size());
     outputFile.write(encryptedData);
 
     outputFile.close();
-
     return true;
 }
 
 bool OpenSSLCryptoManager::DecryptFile(const QString &filePath, const QString &password)
 {
     QFile inputFile(filePath);
-
     if (!inputFile.open(QIODevice::ReadOnly))
         return false;
 
@@ -126,53 +133,35 @@ bool OpenSSLCryptoManager::DecryptFile(const QString &filePath, const QString &p
     QByteArray initializationVector = fileContent.mid(FILE_MAGIC_SIGNATURE.size(), 16);
     QByteArray encryptedData = fileContent.mid(FILE_MAGIC_SIGNATURE.size() + 16);
 
-    EVP_CIPHER_CTX *cipherContext = EVP_CIPHER_CTX_new();
-
+    UniqPtrCipherContext cipherContext(EVP_CIPHER_CTX_new());
     if (!cipherContext)
         return false;
 
-    QByteArray decryptedData(encryptedData.size(), 0);
-
+    std::vector<unsigned char> decryptedData(encryptedData.size() + EVP_MAX_BLOCK_LENGTH);
     int bytesProcessed = 0;
     int totalPlaintextLength = 0;
 
-    if (!EVP_DecryptInit_ex(cipherContext, EVP_aes_256_cbc(), nullptr,
+    if (!EVP_DecryptInit_ex(cipherContext.get(), EVP_aes_256_cbc(), nullptr,
                             reinterpret_cast<const unsigned char *>(decryptionKey.data()),
                             reinterpret_cast<const unsigned char *>(initializationVector.data())))
-    {
-        EVP_CIPHER_CTX_free(cipherContext);
         return false;
-    }
 
-    if (!EVP_DecryptUpdate(cipherContext, reinterpret_cast<unsigned char *>(decryptedData.data()), &bytesProcessed,
+    if (!EVP_DecryptUpdate(cipherContext.get(), decryptedData.data(), &bytesProcessed,
                            reinterpret_cast<const unsigned char *>(encryptedData.data()), encryptedData.size()))
-    {
-        EVP_CIPHER_CTX_free(cipherContext);
         return false;
-    }
-
     totalPlaintextLength = bytesProcessed;
 
-    if (!EVP_DecryptFinal_ex(cipherContext, reinterpret_cast<unsigned char *>(decryptedData.data()) + bytesProcessed,
-                             &bytesProcessed))
-    {
-        EVP_CIPHER_CTX_free(cipherContext);
+    if (!EVP_DecryptFinal_ex(cipherContext.get(), decryptedData.data() + bytesProcessed, &bytesProcessed))
         return false;
-    }
-
     totalPlaintextLength += bytesProcessed;
-
-    EVP_CIPHER_CTX_free(cipherContext);
 
     decryptedData.resize(totalPlaintextLength);
 
     QFile outputFile(filePath);
-
     if (!outputFile.open(QIODevice::WriteOnly))
         return false;
 
-    outputFile.write(decryptedData);
-
+    outputFile.write(reinterpret_cast<char *>(decryptedData.data()), decryptedData.size());
     outputFile.close();
 
     return true;
